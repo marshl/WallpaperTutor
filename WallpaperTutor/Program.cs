68635f0f5136b9e157ff90d0a8bf4f0082dfa8db
marshl <marshl@live.com.au>
2017-05-06 14:50:03 +1000

    Initial commit

--- WallpaperTutor/Program.cs
+++ WallpaperTutor/Program.cs
@@ -1,15 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WallpaperTutor
+﻿namespace WallpaperTutor
 {
-    class Program
+    using System;
+    using System.Diagnostics;
+    using Microsoft.Win32;
+
+    public class Program
     {
         static void Main(string[] args)
         {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Internet Explorer\Desktop\General");
+            if (key == null)
+            {
+                return;
+            }
+
+            object result = key.GetValue("WallpaperSource");
+            if (result == null)
+            {
+                return;
+            }
+
+            string source;
+            try
+            {
+                source = (string)result;
+            }
+            catch (InvalidCastException)
+            {
+                return;
+            }
+
+            if (string.IsNullOrEmpty(source))
+            {
+                return;
+            }
+
+            // Windows Explorer command line arguments: https://support.microsoft.com/en-us/kb/152457
+            Process.Start("explorer", "/select," + source);
         }
     }
 }
